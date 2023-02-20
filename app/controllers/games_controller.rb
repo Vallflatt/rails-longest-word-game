5bd928d8f185b9a0be561aa37d2184af45c3e468
Virginie Allflatt <vallflatt@gmail.com>
2023-02-20 17:32:35 +0100

    finished with styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,25 +19,38 @@ class GamesController < ApplicationController
     @error = ""
 
     #vérifier que le mot est dans les lettres à dispo
-    user_input = @answer.upcase.chars
-    user_input.each do |char|
-      index = letters.index char
-      if index.nil?
-        @success = false
-        @error = "Sorry but #{@answer} can't be built out of #{params[:letters].chars().join(", ")}"
-      else
-        letters.delete_at(index)
-      end
+    if !word_in_letters?(@answer, letters)
+      @success = false
+      @error = "Sorry but #{@answer} can't be built out of #{params[:letters].chars().join(", ")}"
     end
 
     #vérifier que le mot existe si il n'y a pas déjà une erreur
     if @success == true
-      url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
-      result = URI.open(url).read
-      if JSON.parse(result)["found"] == false
+      if !word_exists?(@answer)
         @success = false
         @error = "Sorry but #{@answer} does not seem to be a valid English word..."
       end
     end
+  end
+
+  private
+
+  def word_in_letters?(answer, letters)
+    user_input = answer.upcase.chars
+    user_input.each do |char|
+      index = letters.index char
+      if index.nil?
+        return false
+      else
+        letters.delete_at(index)
+      end
+    end
+    return true
+  end
+
+  def word_exists?(answer)
+    url = "https://wagon-dictionary.herokuapp.com/#{answer}"
+    result = URI.open(url).read
+    return JSON.parse(result)["found"] == true
   end
 end
